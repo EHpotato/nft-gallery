b68112e35ff59b56db95b30bf39328486bf63cbf
Ernesto <eherna65@ucsc.edu>
2021-07-30 15:29:43 -0700

    implemented holy grail layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/home/home';
+import Login from './components/login/login';
 
 const App = () => {
+  // TODO check ethereum browser
+
   return (
-    <div>
-      Hello World
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
