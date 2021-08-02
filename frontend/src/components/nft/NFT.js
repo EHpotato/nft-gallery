cc258571cd0f2642e492eb5bd3c8be9d364c7e48
Ernesto <eherna65@ucsc.edu>
2021-08-02 05:32:41 -0700

    fixed linter errors

--- frontend/src/components/nft/NFT.js
+++ frontend/src/components/nft/NFT.js
@@ -1,22 +1,19 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-const dev_url = "https://go.fission.app/json/3/image.jpg"
+const dev_url = 'https://go.fission.app/json/3/image.jpg';
 
 const NFT = () => {
-
-    useEffect(() => {
-
-    }, []);
-    return (
-        <div>
-            <nav>Collections</nav>
-            {/* TODO: Might have to create separate component for images */}
-            <div>
-                <p>Name of piece | (I) icon</p>
-                <img src={dev_url} alt="cat" width="100%" height="auto"></img>
-            </div>
-        </div>
-    );
+  useEffect(() => {}, []);
+  return (
+    <div>
+      <nav>Collections</nav>
+      {/* TODO: Might have to create separate component for images */}
+      <div>
+        <p>Name of piece | (I) icon</p>
+        <img src={dev_url} alt="cat" width="100%" height="auto"></img>
+      </div>
+    </div>
+  );
 };
 
 export default NFT;
